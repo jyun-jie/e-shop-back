b2a538ed7ca5b7c260e0b5f5308e1ff402173f83
jyun-jie <156783185+jyun-jie@users.noreply.github.com>
2025-12-22 01:32:06 +0800

    refactor : The function shrinkStock replaces getProductQuantityForUpdate in BuyerOrderMapper
    The newly shrinkStock function in BuyerOrderMapper is more efficient.

--- src/main/java/com/shop/mapper/BuyerOrderMapper.java
+++ src/main/java/com/shop/mapper/BuyerOrderMapper.java
@@ -44,7 +44,7 @@ public interface BuyerOrderMapper {
     @Update("update product set quantity = #{quantity} where id = #{ProductId}")
     void updateQuantityByProductId(int ProductId, int quantity);
 
-    @Select("SELECT quantity from product where id = #{ProductId} and status = 'in_stock' FOR UPDATE")
-    int getProductQuantityForUpdate(int ProductId);
+    @Update("update product set quantity = quantity - #{quantity} where id = #{ProductId} AND quantity >= #{quantity}")
+    int shrinkStock(int ProductId , int quantity);
 
 }
