db2e921004cbff2ba581d439702d56353c35fd2e
chen_jie <chenjiegaexpa@gmail.com>
2025-12-26 17:13:04 +0800

    feat : add new function applySeller to request seller authorization .
    add new function applySeller to request seller authorization .

--- src/main/java/com/shop/controller/UserController.java
+++ src/main/java/com/shop/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.shop.controller;
 
 import com.shop.dto.Login;
+import com.shop.dto.SellerApplicationDto;
 import com.shop.entity.AuthenticationResponse;
 import com.shop.entity.Result;
 import com.shop.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -33,7 +35,15 @@ public class UserController {
         return Result.error("查無此用戶或密碼錯誤");
     }
 
-    //logout
+    @PreAuthorize("hasRole('Buyer')")
+    @RequestMapping(method =  RequestMethod.POST , value = "/sellerApplication")
+    public Result applySeller(@RequestBody SellerApplicationDto req ){
+        Boolean resp = userService.applySeller(req) ;
+        if(resp){
+            return  Result.success("申請成功，已進入審核");
+        }
+        return Result.error("申請失敗，有其他原因造成失敗") ;
+    }
 
 
 
